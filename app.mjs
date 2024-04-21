95502a8a0f299d676f831791d07f1e62e89725e2
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-04-21 12:44:45 -0400

    Add weather route to app
    Put node app.mjs in an open terminal, this opens localhost. to get to the template weather page you need to actually add /weather to URL to get the html page to display, working on actually adding functionality to switch between both

--- app.mjs
+++ app.mjs
@@ -1,16 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
 const app = express();
 
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-app.get('/login', (req, res) => {
-    // login html page will be rendered here
-    console.log('test');
+app.get('/', (req, res) => {
+    res.send("login goes here");
 });
+
+app.get('/weather', (req, res) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const _retfile = path.join(__dirname, 'weather.html');
+
+    res.sendFile(_retfile);
+});
+
 
 app.listen(port, () => {
     console.log('Running on: http://localhost:3000');
